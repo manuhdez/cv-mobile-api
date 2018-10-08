cb95dca1113462eb57da028451d5a8d97ac894ed
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-10-08 20:28:11 +0100

    added post route to create new users

--- models/user.js
+++ models/user.js
@@ -4,13 +4,15 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   firstName: {type: String, required: true},
   lastName: {type: String, required: true},
+  username: {type: String, required: true},
   email: {type: String, unique: true, required: true},
-  location: {type: Array, required: true},
+  location: {type: Object, required: true},
   phoneNumber: Number,
   website: {type: String, trim: true},
   languages: Array,
   skills: Array,
-  profilePicture: {type: String, required: true}
+  gender: {type: String, required: true},
+  profilePicture: String
 });
 
 const User = mongoose.model('User', UserSchema);
--- routes/api.js
+++ routes/api.js
@@ -14,4 +14,22 @@ router.get('/users', (req, res, next) => {
   User.find().then( users => res.json(users));
 });
 
+router.post('/users', (req, res, next) => {
+  const newUser = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    username: req.body.username,
+    email: req.body.email,
+    location: {...req.body.location},
+    phoneNumber: req.body.phoneNumber,
+    website: req.body.website,
+    languages: [...req.body.languages],
+    skills: [...req.body.skills]
+  }
+
+  User.create(newUser, (err) => {
+    if (err) return next(err);
+  })
+});
+
 module.exports = router;
