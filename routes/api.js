09e051f9177953bed1bf22757ae795b8e101de48
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-10-09 16:04:36 +0100

    mixed query with main users route

--- routes/api.js
+++ routes/api.js
@@ -36,15 +36,14 @@ router.get('/', (req, res, next) => {
 
 // Get all users
 router.get('/users', (req, res, next) => {
-  User.find().then( users => res.json(users));
-});
-
-// Get a page with 10 users
-router.get('/users/page/:pageId', (req, res, next) => {
-  User.find()
-    .skip((req.params.pageId - 1) * 10)
+  if (req.query.page) {
+    User.find()
     .limit(10)
+    .skip((req.params.pageId - 1) * 10)
     .then( users => res.json(users));
+  } else {
+    User.find().then( users => res.json(users));
+  }
 });
 
 // Add a new user to the database
