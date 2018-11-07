fa3444ca5ccbabba59957ce871a94413916cbd84
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-11-07 19:30:16 +0000

    removed unnecesary properties from langs schema

--- models/lang.js
+++ models/lang.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const LanguageSchema = new Schema({
-  name: {type: String, required: true},
   label: {type: String, required: true},
-  value: {type: String, required: true},
-  default: {type: Number, required: true}
 });
 
 const Language = mongoose.model('Language', LanguageSchema);
