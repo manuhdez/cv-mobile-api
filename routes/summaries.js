5724ea7b3b725b5cca5f5ff004a2900832f38e08
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-11-13 13:04:49 +0000

    temparary changes

--- routes/summaries.js
+++ routes/summaries.js
@@ -12,8 +12,36 @@ exports.get = (req, res, next) => {
 exports.getByOrigin = (req, res, next) => {
   console.log(req.params.origin);
   Summary
-    .find({'origin': req.params.origin})
+    .findOne({'origin': req.params.origin})
     .populate('origin')
-    .then( summary => res.json(summary))
+    .then( summary => res.json(summary[0]))
     .catch( err => next(err));
+}
+
+exports.updateSummary = (req, res, next) => {
+  // Recibir un objeto con las respuestas al formulario
+  let userResponse = {...req.body}
+  // Buscar a que valores corresponden esas respuestas y sumarles 1 al resultado
+  Summary.find({origin: req.params.origin})
+    .then( sum => {
+      let { _id, origin, title, totalAnswers, answers, } = sum[0];
+
+      let updatedAnswers = [];
+
+      let updatedSummary = {
+        _id,
+        origin,
+        title,
+        totalAnswers: totalAnswers += 1,
+        answers: updatedAnswers
+      }
+      // updatedSummary.totalAnswers = parseInt(updatedSummary.totalAnswers) + 1;
+      res.json(updatedSummary);
+      // updatedSummary.answers.forEach( answer => {
+
+      // })
+    })
+    .catch( err => next(err));
+  // Guardar el resumen actualizado en mongodb
+
 }
