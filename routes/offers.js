92d66dd7eb7d659297415024103a3baecec93ceb
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-11-27 20:33:41 +0000

    added authentication to every offer route

--- routes/offers.js
+++ routes/offers.js
@@ -15,64 +15,55 @@ exports.get = (req, res, next) => {
 };
 
 exports.getById = (req, res, next) => {
-  Offer
-    .findById(req.params.id)
-    .then( (offer) => res.json(offer) )
-    .catch( err => next(err));
+  jwt.verify(req.token, 'secret_key', (err, tokenData) => {
+    if (err) return next(err);
+
+    Offer
+      .findById(req.params.id)
+      .then( (offer) => res.json(offer) )
+      .catch( err => next(err));
+  });
 };
 
 exports.add = (req, res, next) => {
-  let { title,
-    contractType,
-    location,
-    description,
-    companyEmail,
-    company,
-    responsabilities,
-    whatWeOffer,
-    whatWeLookFor } = req.body;
+  jwt.verify(req.token, 'secret_key', (err, tokenData) => {
 
-  const newOffer = {
-    companyEmail,
-    company,
-    title,
-    contractType,
-    location,
-    description,
-    responsabilities,
-    whatWeOffer,
-    whatWeLookFor
-  }
+    let { title, contractType, location, description, companyEmail, company, responsabilities, whatWeOffer, whatWeLookFor } = req.body;
 
-  // Check if the email sent belong to an existing company
-  Company
-    .find({email: companyEmail})
-    .then( doc => {
-      if (doc.length === 0) return res.json({message: 'Please use an existing company email'});
+    const newOffer = { companyEmail, company, title, contractType, location, description, responsabilities, whatWeOffer, whatWeLookFor };
 
-      Offer.create(newOffer, (err, offer) => {
-        if (err) return next(err);
-        if (!offer) {
-          const error = new Error('Something went wrong.');
-          error.status(500);
-          return next(error);
-        }
+    // Check if the email sent belong to an existing company
+    Company
+      .find({email: companyEmail})
+      .then( doc => {
+        if (doc.length === 0) return res.json({message: 'Please use an existing company email'});
 
-        Company
-          .update({email: companyEmail}, {$push: {jobOffers: offer._id}})
-          .then(() => res.json(offer))
-          .catch( err => next(err));
-      });
-    })
-    .catch( err => next(err));
+        Offer.create(newOffer, (err, offer) => {
+          if (err) return next(err);
+          if (!offer) {
+            const error = new Error('Something went wrong.');
+            error.status(500);
+            return next(error);
+          }
+
+          Company
+            .update({email: companyEmail}, {$push: {jobOffers: offer._id}})
+            .then(() => res.json(offer))
+            .catch( err => next(err));
+        });
+      })
+      .catch( err => next(err));
+  });
 };
 
 exports.delete = (req, res, next) => {
-  Offer.findByIdAndDelete(req.params.id, (err, offer) => {
-    if (err) return next(err);
-    Company
-      .findOneAndUpdate({email: offer.companyEmail}, {$pull: {jobOffers: offer._id}})
-      .then(() => res.status(200).json({message: 'Offer successfuly deleted'}))
-      .catch( err => next(err));
+  jwt.verify(req.token, 'secret_key', (err, tokenData) => {
+    Offer.findByIdAndDelete(req.params.id, (err, offer) => {
+      if (err) return next(err);
+      Company
+        .findOneAndUpdate({email: offer.companyEmail}, {$pull: {jobOffers: offer._id}})
+        .then(() => res.status(200).json({message: 'Offer successfuly deleted'}))
+        .catch( err => next(err));
+    });
   });
 };
