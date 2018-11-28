71d79521f284dec89be2b1e51d56cc68d18389ee
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-11-28 19:19:20 +0000

    added alternatives to authenticate via user or via company

--- routes/login.js
+++ routes/login.js
@@ -1,20 +1,38 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/user';
+import Company from '../models/company';
 
 exports.login = (req, res, next) => {
+  if (!req.body.type) return res.status(401).json({message: 'Authentication error'});
 
-  User
-    .findOne({email: req.body.email, username: req.body.username})
-    .then((response) => {
-      jwt.sign({user: response._id}, 'secret_key', {expiresIn: '30m'}, (err, token) => {
-        if (err) return next(err);
+  if (req.body.type === 'user') {
+    User
+      .findOne({email: req.body.email, username: req.body.username})
+      .then((response) => {
+        jwt.sign({user: response._id}, 'secret_key', {expiresIn: '30m'}, (err, token) => {
+          if (err) return next(err);
 
-        res.status(200).json({
-          message: `Welcome back, ${response.name}`,
-          token,
-          id: response._id
+          res.status(200).json({
+            message: `Welcome back, ${response.name}`,
+            token,
+            id: response._id
+          });
+        });
+      })
+      .catch(() => res.status(400).json({message: 'username or email not valid'}));
+  } else if (req.body.type === 'company') {
+    Company
+      .findOne({email: req.body.email, docNumber: req.body.password})
+      .then((response) => {
+        jwt.sign({user: response._id}, 'secret_key', {expiresIn: '30m'}, (err, token) => {
+          if (err) return next(err);
+
+          res.status(200).json({
+            message: `Welcome back ${response.name}`,
+            token,
+            id: response._id
+          });
         });
       });
-    })
-    .catch(() => res.status(400).json({message: 'username or email not valid'}));
+  }
 }
